c97f8c4eed1d3b2d40d27cb26ce32b4b50de4b93
Kaysie Anderson <kaysielandersonwork@gmail.com>
2023-02-11 19:58:03 -0600

    new project

---
+++ src/main/java/com/anderson/trakd/TrakdApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.trakd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrakdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anderson/trakd/TrakdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.trakd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrakdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
