9141b6810386b3e04d4e00fc59a3741641c6b343
Kaysie Anderson <kaysielandersonwork@gmail.com>
2023-02-20 08:59:09 -0600

    added schema.sql and schema diagram

---
+++ src/main/resources/static/assets/schema/schema.sql
@@ -0,0 +1,19 @@
+
+# INSERT INTO dept (name) VALUES ('Front-End');
+# INSERT INTO dept (name) VALUES ('Back-End');
+# INSERT INTO dept (name) VALUES ('QA');
+# INSERT INTO dept (name) VALUES ('Security');
+# INSERT INTO dept (name) VALUES ('Data');
+#
+# INSERT INTO manager (name) VALUES ('Chris, Nobles');
+# INSERT INTO manager (name) VALUES ('Richard, Hastings');
+# INSERT INTO manager (name) VALUES ('Sarah, Lee');
+# INSERT INTO manager (name) VALUES ('Michael, Donnelly');
+# INSERT INTO manager (name) VALUES ('Amber, Pearl');
+#
+#
+# UPDATE manager SET dept_id = 5  WHERE id = 1;
+# UPDATE manager SET dept_id = 3  WHERE id = 2;
+# UPDATE manager SET dept_id = 1  WHERE id = 3;
+# UPDATE manager SET dept_id = 2  WHERE id = 4;
+# UPDATE manager SET dept_id = 4  WHERE id = 5;
